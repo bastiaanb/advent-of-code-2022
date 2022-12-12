53246df97fca9de34b9ab16ce4fa5e353c154850
Bastiaan Bakker <bbakker@xebia.com>
2022-12-12 17:53:49 +0100

    simplify a bit more

--- day-12/star23-24.py
+++ day-12/star23-24.py
@@ -10,19 +10,16 @@ def calc_path_length(heights, start, end):
     distance = np.full_like(heights, -1)
     distance[start] = 0
 
-    for step in range(heights.size):
-        at_step = np.where(distance == step)
-        for i in range(len(at_step[0])):
-            c = (at_step[0][i], at_step[1][i])
+    for step in range(1, heights.size):
+        for i in np.argwhere(distance == step - 1):
             for d in directions:
-                c2 = (c[0] + d[0], c[1] + d[1])
-                if c2[0] >= 0 and c2[0] < heights.shape[0] and c2[1] >=0 and c2[1] < heights.shape[1]:
-                    if distance[c2] == -1:
-                        if (heights[c2] - heights[c] <= 1):
-                            if c2 == end:
-                                return step + 1
-                            distance[c2] = step + 1
-
+                c = tuple(i + d)
+                if c >= (0, 0) and c[0] < heights.shape[0] and c[1] < heights.shape[1]:
+                    if distance[c] == -1:
+                        if (heights[c] - heights[tuple(i)] <= 1):
+                            if c == end:
+                                return step
+                            distance[c] = step
 
 heights = np.array([
     [ ord(c) for c in line.rstrip() ]
@@ -34,10 +31,9 @@ end = np.where(heights == ord('E'))
 
 heights[start] = ord('a')
 heights[end] = ord('z')
-heights -= ord('a')
 
 star23 = calc_path_length(heights, start, end)
 print(f"star 23: {star23}")
 
-star24 = calc_path_length(heights, np.where(heights == 0), end)
+star24 = calc_path_length(heights, np.where(heights == ord('a')), end)
 print(f"star 24: {star24}")
